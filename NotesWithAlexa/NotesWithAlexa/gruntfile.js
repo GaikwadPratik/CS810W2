15cb527a79e24b0e6a599a46c05977437426b4f6
orion <pratik.gaikwad19@gmail.com>
2017-07-21 17:54:49 -0400

    To create dist folder for publishing

---
+++ NotesWithAlexa/NotesWithAlexa/gruntfile.js
@@ -0,0 +1,43 @@
+﻿'use strict'
+module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        src: ["package.json", "Procfile", "app.json"],
+                        dest: "./dist"
+                    }
+                ]
+            }
+        },
+        ts: {
+            app: {
+                files: [
+                    {
+                        src: ["*.ts", "!node_modules/**"],
+                        dest: "./dist"
+                    }
+                ],
+                options: {
+                    module: "commonjs",
+                    target: "es6",
+                    sourceMap: true
+                }
+            }
+        },
+        watch: {
+            ts: {
+                files: ["*.ts"],
+                tasks: ["ts", "copy"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-ts");
+
+    grunt.registerTask("default", ["copy", "ts"]);
+}
